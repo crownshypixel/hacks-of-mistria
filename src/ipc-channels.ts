d9352919819f162fe0c5f2fcde8dc85fee9e3e15
crownshypixel <crownshypixel@gmail.com>
2024-11-03 17:12:25 +0200

    migrate to electron-vite

--- src/ipc-channels.ts
+++
@@ -1,5 +0,0 @@
-export const IPC_EVENTS = {
-  GET_SAVE_METADATA: "get-save-metadata",
-  GET_SAVE_NAMES: "get-save-names",
-  REFRESH_SAVES: "refresh-saves",
-};
--- src/renderer.ts
+++
@@ -1,9 +0,0 @@
-import type {API} from './preload'
-
-declare global {
-  interface Window {
-    api: API
-  }
-}
-
-console.log("Hello World2")
