b508d6b4e7481e442f8dd68eec1b0426ec493f2d
crownshypixel <crownshypixel@gmail.com>
2024-11-23 01:49:06 +0200

    add dozy

--- src/renderer/src/app.jsx
+++ src/renderer/src/app.jsx
@@ -4,6 +4,7 @@ import SaveSelection from "@components/save-selection"
 import SaveEditing from "@components/save-editing"
 import fomClouds from "@assets/fom-clouds.png"
 import fomLogo from "@assets/fom-logo.webp"
+import dozy from "@assets/dozy.webp"
 
 function Layout({ children }) {
   return (
@@ -35,7 +36,19 @@ export function App() {
       {editingSaveId ? (
         <SaveEditing saveId={editingSaveId} onBack={goToSelection} />
       ) : (
-        <SaveSelection onSaveSelected={goToEditing} />
+        <>
+          <SaveSelection onSaveSelected={goToEditing} />
+          <Image
+            src={dozy}
+            zIndex={2}
+            draggable={false}
+            pos="absolute"
+            bottom="10"
+            right="10"
+            transform="scaleX(-1)"
+          />
+          <Image src={dozy} zIndex={2} draggable={false} pos="absolute" bottom="10" left="10" />
+        </>
       )}
     </Layout>
   )
